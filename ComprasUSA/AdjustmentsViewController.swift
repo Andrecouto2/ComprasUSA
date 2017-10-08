99718750a705c1614eeaa9a9e5b0f4ad79f6f6f1
André Couto <andrecouto@mac-mini-de-andre.local>
2017-10-08 14:58:16 -0300

    UIAlertController add

--- ComprasUSA/AdjustmentsViewController.swift
+++ ComprasUSA/AdjustmentsViewController.swift
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+import CoreData
+
+enum StateType {
+    case add
+    case edit
+}
 
 class AdjustmentsViewController: UIViewController {
 
@@ -14,8 +20,49 @@ class AdjustmentsViewController: UIViewController {
     @IBOutlet weak var tfIof: UITextField!
     @IBOutlet weak var tbState: UITableView!
     
+    // MARK: - Properties
+    var dataSource: [State] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tbState.delegate = self
+        tbState.dataSource = self
+        loadStates()
+    }
+    
+    func loadStates() {
+        let fetchRequest: NSFetchRequest<State> = State.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        do {
+            dataSource = try context.fetch(fetchRequest)
+            tbState.reloadData()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func showAlert(type: StateType, state: State?) {
+        let title = (type == .add) ? "Adicionar" : "Editar"
+        let alert = UIAlertController(title: "\(title) Estado", message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField: UITextField) in
+            textField.placeholder = "Nome do estado"
+            if let name = state?.name {
+                textField.text = name
+            }
+        }
+        alert.addAction(UIAlertAction(title: title, style: .default, handler: { (action: UIAlertAction) in
+            let state = state ?? State(context: self.context)
+            state.name = alert.textFields?.first?.text
+            do {
+                try self.context.save()
+                self.loadStates()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -25,6 +72,27 @@ class AdjustmentsViewController: UIViewController {
     }
 
     @IBAction func AddState(_ sender: UIButton) {
-        
+        showAlert(type: .add, state: nil)
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension AdjustmentsViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDelegate
+extension AdjustmentsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let state = dataSource[indexPath.row]
+        cell.textLabel?.text = state.name
+        cell.detailTextLabel?.text = String(state.tax)
+        return cell
     }
 }
